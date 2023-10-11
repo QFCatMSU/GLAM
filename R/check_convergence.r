3dd88233099b7030063749845822fc37e9e4ced0
lidach <lisa.chong8594@gmail.com>
2023-10-11 11:30:20 -0400

    modify check_convergence to run with run_glam, run_glam will execute GLAM

--- R/check_convergence.r
+++ R/check_convergence.r
@@ -15,15 +15,22 @@ check_convergence = function(
   res = list()
   res$convergence = model_res$convergence
   res$max_gradient = max(abs(obj_fn$gr(model_res$par)))
+  final_gradient = obj_fn$gr(model_res$par)
 
   if (res$convergence == 1) {
-    print(res$convergence)
     message("Model did not converge!")
   }
   if (res$max_gradient > 0.001) {
-    gr = cbind(names(obj_fn$env$last.par.best), c(obj_fn$gr(obj_fn$env$last.par.best)))
-    print(gr[which(as.numeric(gr[,2]) > 0.001),])
-    stop("Gradients are high, please improve optimization!")
+    res$whichbad_params = data.frame(
+      parameters = names(model_res$par),
+      MLE = as.numeric(model_res$par),
+      gradient = as.numeric(final_gradient),
+      parameter_check = c(ifelse(as.numeric(final_gradient) > 0.001, "Bad", "OK"))
+    )
+    message("Gradients are high, please improve optimization!")
+    return(res)
+  } else {
+    res$whichbad_params = "All parameter gradients look good!"
   }
   # look at fixed estimated parameters
   if (length(obj_fn$env$random) == 0) {
@@ -34,30 +41,25 @@ check_convergence = function(
   # extract parameters and uncertainty
   res$Hess = optimHess(par = fixed_obj, fn = obj_fn$fn, gr = obj_fn$gr)
   if (is.nan(max(res$Hess))) {
-    print("The hessian was not invertible")
+    res$hess_status = "The hessian was not invertible"
   } else {
     res$eigen = eigen(res$Hess)
-    res$which_bad = which(res$eigen$values < sqrt(.Machine$double.eps))
+    res$whichbad_eigen = which(res$eigen$values < sqrt(.Machine$double.eps))
     # check for parameters
-    if (length(res$eigen$vectors[, res$which_bad]) > 0) {
-      rowmax = apply(as.matrix(res$eigen$vectors[, res$which_bad]),
-        MARGIN = 1, FUN = function(x){max(abs(x))})
+    if (length(res$eigen$vectors[, res$whichbad_eigen]) > 0) {
+      rowmax = apply(as.matrix(res$eigen$vectors[, res$whichbad_eigen]),
+        MARGIN = 1, FUN = function(x){ max(abs(x)) })
+
+      res$whichbad_eigen = data.frame(
+          "Parameters" = names(obj_fn$par),
+          "MLE" = fixed_obj,
+          "Parameter_check" = ifelse(rowmax > 0.001, "Bad", "OK")
+          )
     } else {
-      rowmax = rep(0, length(res$eigen$values))
-    }
-    res$bad_params = data.frame(
-      "Parameters" = names(obj_fn$par),
-      "MLE" = fixed_obj,
-      "Parameter_check" = ifelse(rowmax > 0.001, "Bad", "OK")
-    )
-    # message for parameters
-    if (length(res$which_bad) == 0) {
-      message("All parameters are identifiable")
-    } else {
-      message("There are some parameters that were not identifiable: check parameter list!")
+      res$whichbad_eigen = "All parameters are identifiable"
     }
   }
-  if(res$convergence == 0 & res$max_gradient < 0.001 & length(res$which_bad) == 0){
+  if(res$convergence == 0 & res$max_gradient < 0.001 & class(res$whichbad_eigen) == "character"){
     message("Model diagnostics consistent with convergence.")
     res <- list()
     res$convergence = model_res$convergence
--- R/run_glam.R
+++ R/run_glam.R
@@ -1,10 +1,105 @@
 #' @title run_glam
 #' 
-#' @description 
+#' @description run GLAM with nlminb and Newton steps
+#'
+#' @param nlminb_control
+#' @param fixed_names names of fixed parameters that will go in map argument of MakeADFun
+#' @param rand_names ## ** not used right now
+#' @param bound_list ## ** not used right now
+#' @param report_sdrep use sdreport from TMB, get standard errors for parameters
+#' @param n_newton number of Newton steps (recommended max = 3)
+
 #' 
-#' @param data list of data
-#' @param pars list of parameters
+#' @return list of model diagnostics related to convergence and gradients, model results, and parameter estimates
 #' 
-run_glam = function(pars) {
-  
+
+run_glam = function(nlminb_control = list(
+                      eval.max = 1e3,
+                      iter.max = 1e3,
+                      trace = 0
+                    ),
+                    fixed_names = NULL,
+                    # rand_names = NULL,
+                    # bound_list = NULL,
+                    report_sdrep = TRUE,
+                    n_newton = 3) {
+  # mapping fixed parameters and removing bounds if parameter is fixed
+  if (!is.null(fixed_names)) {
+    fixed_list = list()
+    for (i in 1:length(fixed_names)) {
+      fixed_list[[paste(fixed_names[i])]] = as.factor(rep(NA, length(pars[[which(names(pars) %in% fixed_names[i])]])))
+    }
+  } else {
+    fixed_list = NULL
+  }
+
+
+  ## MakeADFun ####
+  model = MakeADFun(func = glam, parameters = pars, map = fixed_list, hessian = TRUE, silent = TRUE)
+
+  ## ** - will need to incorporate bounds and random effects later
+
+  ## Run model ####
+  res = try(nlminb(model$par, model$fn, model$gr,
+    control = nlminb_control
+  ))
+
+
+  ## Check model convergence and gradients ####
+  # rerun model with Newton steps if gradients are bad
+  final_gradient = model$gr(res$par)
+  max_gradient = max(abs(final_gradient))
+  if (max_gradient < 0.001) {
+    n_newton_steps = 0
+  } else {
+    n_newton_steps = 1
+  }
+  if (n_newton_steps) {
+    tryCatch(
+      for (n in 1:n_newton) {
+        g = as.numeric(model$gr(res$par))
+        h = stats::optimHess(res$par, model$fn, model$gr) # Hessian matrix
+        new_par = res$par - solve(h, g)
+        # rewrite results
+        res = nlminb(new_par, model$fn, model$gr,
+          control = list(eval.max = 1e4, iter.max = 1e4)
+        )
+      }, error = function(e) { err = conditionMessage(e) }
+    )
+  }
+
+  # look at convergence, gradients, Hessian
+  check = check_convergence(obj_fn = model, model_res = res)
+
+
+  ## Report ####
+  # sdreport
+  if (report_sdrep) {
+    sdrep = try(sdreport(model))
+    sdrep = summary(sdrep)
+  } else {
+    sdrep = NULL
+  }
+
+  # saved parameters
+  df = tryCatch(
+    data.frame(
+      "parameter" = names(res$par),
+      "estimate" = res$par,
+      "gradient" = as.vector(final_gradient)
+    ),
+    error = function(e) {
+      return(NULL)
+    }
+  )
+
+  # export
+  output = list()
+    output$check = check # has all model convergence, gradient, and Hessian checks and messages, check this after model run
+    output$report = model$report(res$par) # list of output from RTMB model
+    output$params = df # parameter list with parameter names, estimates, and gradients
+    output$sdrep = sdrep # sdreport output - parameter estimates and standard errors
+    output$model = model # MakeADFun model/output
+
+  return(output)
 }
